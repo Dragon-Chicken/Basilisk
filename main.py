f2f0804aa2b8a4dc94e79864474aeec860289778
Dragon-Chicken <70321204+Dragon-Chicken@users.noreply.github.com>
2023-05-01 11:27:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def init_engine():
     engine.import_image("Assets/paddle.png", "paddle")
     engine.import_image("Assets/ball.png", "ball")
 
+    engine.import_sfx("Assets/Hit.wav", "ball_hit")
+    engine.import_sfx("Assets/Score.wav", "score")
+
 
 def update():
     global paddle_y
@@ -37,31 +40,37 @@ def update():
 
     if ball_y > 60:
         dy = -0.5
+        engine.play_sfx("ball_hit")
     if ball_y < 0:
         dy = 0.5
-    if ball_x > 60:
+        engine.play_sfx("ball_hit")
+    if ball_x > 61:
         dx = -0.5
         ball_x = 30
         ball_y = 30
         score -= 1
+        engine.play_sfx("score")
     if ball_x < 0:
         dx = 0.5
         ball_x = 30
         ball_y = 30
         score += 1
+        engine.play_sfx("score")
 
     if ball_x > 54 and paddle_y < ball_y < paddle_y + 16:
         dx = -0.5
+        engine.play_sfx("ball_hit")
     if ball_x < 6 and paddle2_y < ball_y < paddle2_y + 16:
         dx = 0.5
+        engine.play_sfx("ball_hit")
 
-    if ball_y < paddle2_y + 8: paddle2_y -= 0.39
-    if ball_y > paddle2_y + 8: paddle2_y += 0.39
+    if ball_y < paddle2_y + 8: paddle2_y -= 0.42
+    if ball_y > paddle2_y + 8: paddle2_y += 0.42
 
 
 def draw():
     engine.screen.fill("#181425")
-    engine.text(score, 30, 0)
+    engine.text(score, 30, 1)
     engine.draw("paddle", 58, paddle_y)
     engine.draw("paddle", 2, paddle2_y)
     engine.draw("ball", ball_x, ball_y)
