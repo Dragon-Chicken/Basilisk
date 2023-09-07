43f38513b7efa295c93811d68865bf61f16a0397
Ritshidze Nemudzivhadi <49242067+Xlt3h@users.noreply.github.com>
2023-09-07 18:37:28 +0200

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from sys import platform
+import os
+if platform == "linux" or platform == "linux2":
+    # linux
+    os.system("pip3 install -r requirements.txt")
+elif platform == "darwin":
+    # OS X
+    os.system("pip3 install -r requirements.txt")
+elif platform == "win32":
+    os.system("pip install -r requirements.txt")
