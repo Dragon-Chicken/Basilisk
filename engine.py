8dfb1cc7b5aef381a70af9758ab694bdff824d43
Ritshidze Nemudzivhadi <49242067+Xlt3h@users.noreply.github.com>
2023-09-07 18:32:01 +0200

    fixed running

--- engine.py
+++ engine.py
@@ -1,169 +1,144 @@
-# Basilisk Engine
-# Version: 1.1
-
-import sys
 import pygame
-import main as main
+import sys
 
-pygame.init()
-display = pygame.display.set_mode((640, 640))
-pygame.display.set_caption("Pong")
-pygame.display.set_icon(pygame.image.load("Engine requirements/Basilisk.png"))
-screen = pygame.Surface((64, 64))
+class engine_:
+    
+    def __init__(self) -> None:
+        pygame.init()
+        self.display = pygame.display.set_mode((640, 640))
+        pygame.display.set_caption('game')
+        pygame.display.set_icon(pygame.image.load("Engine requirements/Basilisk.png"))
+        self.screen = pygame.Surface((64, 64))
+        self.font = pygame.font.Font("Engine requirements/Pixel-type.ttf", 15)
+        self.images = []
+        self.SFXs = []
+        self.mouse_scroll = 0
 
+    def pass_down(self, initialize, draw_, update):
+        self.initialize = initialize
+        self.draw_ = draw_
+        self.update = update
 
-font = pygame.font.Font("Engine stuff/Pixel-type.ttf", 15)
+    def import_sfx(self, sfx, sfx_name):
+        self.sfx_name = sfx_name.lower()
+        self.SFXs.append([sfx_name, pygame.mixer.Sound(sfx)]) 
 
-images = []
-SFXs = []
+    def play_sfx(self, sfx_name):
+        self.sfx_name = sfx_name.lower()
+        for sfx in self.SFXs:
+            if sfx[0] == self.sfx_name:
+                sfx[1].play()
 
-mouse_scroll = 0
+    def key_down(self, key) -> bool:
+        #self.key = "pygame." + key
+        #self.keys = pygame.key.get_pressed()
+        #if self.keys[eval(key)]:
+            #return True
+        key = getattr(pygame, key)  # Get the Pygame key constant
+        self.keys = pygame.key.get_pressed()
+        if self.keys[key]:
+            return True
 
-# sfx
+    def movement(self, x, y, movement_speed) -> int:
+        if self.key_down("K_w") or self.key_down("K_UP"):
+            y -= movement_speed
+        if self.key_down("K_s") or self.key_down("K_DOWN"):
+            y += movement_speed
+        if self.key_down("K_a") or self.key_down("K_LEFT"):
+            x -= movement_speed
+        if self.key_down("K_d") or self.key_down("K_RIGHT"):
+            x += movement_speed
 
+        return x, y
 
-def import_sfx(sfx, sfx_name):
-    global SFXs
-    sfx_name = sfx_name.lower()
-    SFXs.append([sfx_name, pygame.mixer.Sound(sfx)])
+    def mouse_clicks(self, mouse_button):
+        self.left, self.middle, self.right = pygame.mouse.get_pressed()
+        if mouse_button == "left" or mouse_button == "l" and self.left:
+            return True
+        elif mouse_button == "middle" or mouse_button == "m" and self.middle:
+            return True
+        elif mouse_button == "right" or mouse_button == "r" and self.right:
+            return True
+        elif mouse_button == "all":
+            return self.left, self.middle, self.right
 
+    def mouse_pos(self):
+        x, y = pygame.mouse.get_pos()
+        return int(x / 10), int(y / 10)
 
-def play_sfx(sfx_name):
-    global SFXs
-    sfx_name = sfx_name.lower()
-    for sfx in SFXs:
-        if sfx[0] == sfx_name:
-            sfx[1].play()
+    def tile_map(self, tiles):
+        if tiles is None:
+            tiles = []
+        i = 0
+        for y in range(0, 8):
+            for x in range(0, 8):
+                self.draw(str(tiles[i]), x * 8, y * 8)
+                i += 1
 
-# update
+    def image_import(self, image, image_name):
+        self.object_name = image_name.lower()
+        self.images.append([self.object_name, pygame.image.load(image).convert_alpha()])
 
+    def draw(self, object_name, x, y):
+        object_name = object_name.lower()
+        for image in self.images:
+            if image[0] == object_name:
+                surf = image[1]
+                rect = surf.get_rect(topleft=(x, y))
+                self.screen.blit(surf, rect)
 
-def key_down(key):
-    key = "pygame." + key
-    keys = pygame.key.get_pressed()
-    if keys[eval(key)]:
-        return True
+    def draw_rect(self, tx, ty, bx, by, colour=(255, 255, 255)):
+        self.draw_line(tx, ty, by, ty, colour)
+        self.draw_line(bx, ty, bx, by, colour)
+        self.draw_line(bx, by, tx, by, colour)
+        self.draw_line(tx, by, tx, ty, colour)
 
+    def draw_rect_filled(self, tx, ty, bx, by, colour=(255, 255, 255)):
+        bx -= tx
+        by -= ty
+        pygame.draw.rect(self.screen, colour, (tx, ty, bx, by))
 
-def movement(x, y, movement_speed):
-    if key_down("K_w") or key_down("K_UP"):
-        y -= movement_speed
-    if key_down("K_s") or key_down("K_DOWN"):
-        y += movement_speed
-    if key_down("K_a") or key_down("K_LEFT"):
-        x -= movement_speed
-    if key_down("K_d") or key_down("K_RIGHT"):
-        x += movement_speed
+    def draw_line(self, x1, y1, x2, y2, colour=(255, 255, 255)):
+        pygame.draw.line(self.screen, colour, (x1, y1), (x2, y2), 1)
 
-    return x, y
+    def text(self, text_to_display, x, y, colour=(255, 255, 255)):
+        surf = self.font.render(f"{text_to_display}", False, colour)
+        rect = surf.get_rect(topleft=(x, y))
+        self.screen.blit(surf, rect)
 
+    def run(self):
+        self.clock = pygame.time.Clock()
+        self.initialize()
+        self.z = 0
+        self.screen.fill((0, 0, 0))
+        self.screen = pygame.transform.scale(self.screen, (64, 64))
+        
+        while True:
+            dt = self.clock.tick(10000) / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEWHEEL:
+                    self.mouse_scroll = event.y
 
-def mouse_clicks(mouse_button):
-    left, middle, right = pygame.mouse.get_pressed()
-    if mouse_button == "left" or mouse_button == "l" and left:
-        return True
-    elif mouse_button == "middle" or mouse_button == "m" and middle:
-        return True
-    elif mouse_button == "right" or mouse_button == "r" and right:
-        return True
-    elif mouse_button == "all":
-        return left, middle, right
+            if self.z > 0.034:
+                print("***********************************\n"
+                      "WARNING RUNNING AT LESS THAN 30 FPS\n"
+                      "***********************************")
 
+            if self.z >= 0.033:
+                self.update()
+                self.update()
+                self.z = 0
+            elif self.z >= 0.016:
+                self.update()
+                self.z = 0
+            else:
+                self.z += 1 * dt
 
-def mouse_pos():
-    x, y = pygame.mouse.get_pos()
-    return int(x/10), int(y/10)
+            self.draw_()
 
-
-# draw
-
-
-def tile_map(tiles):
-    if tiles is None:
-        tiles = []
-    i = 0
-    for y in range(0, 8):
-        for x in range(0, 8):
-            draw(str(tiles[i]), x*8, y*8)
-            i += 1
-
-
-def image_import(image, image_name):
-    global images
-    object_name = image_name.lower()
-    images.append([object_name, pygame.image.load(image).convert_alpha()])
-
-
-def draw(object_name, x, y):
-    global images
-    object_name = object_name.lower()
-    for image in images:
-        if image[0] == object_name:
-            surf = image[1]
-            rect = surf.get_rect(topleft=(x, y))
-            screen.blit(surf, rect)
-
-
-def draw_rect(tx, ty, bx, by, colour=(255, 255, 255)):
-    draw_line(tx, ty, by, ty, colour)
-    draw_line(bx, ty, bx, by, colour)
-    draw_line(bx, by, tx, by, colour)
-    draw_line(tx, by, tx, ty, colour)
-
-
-def draw_rect_filled(tx, ty, bx, by, colour=(255, 255, 255)):
-    bx -= tx
-    by -= ty
-    pygame.draw.rect(screen, colour, (tx, ty, bx, by))
-
-
-def draw_line(x1, y1, x2, y2, colour=(255, 255, 255)):
-    pygame.draw.line(screen, colour, (x1, y1), (x2, y2), 1)
-
-
-def text(text_to_display, x, y, colour=(255, 255, 255)):
-    surf = font.render(f"{text_to_display}", False, colour)
-    rect = surf.get_rect(topleft=(x, y))
-    screen.blit(surf, rect)
-
-
-main.init()
-
-clock = pygame.time.Clock()
-
-z = 0
-
-while True:
-
-    screen = pygame.transform.scale(screen, (64, 64))
-    screen.fill((0, 0, 0))
-
-    dt = clock.tick(10000) / 1000
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEWHEEL:
-            mouse_scroll = event.y
-
-    if z > 0.034:
-        print("***********************************\n"
-              "WARNING RUNNING AT LESS THAN 30 FPS\n"
-              "***********************************")
-
-    if z >= 0.033:
-        main.update()
-        main.update()
-        z = 0
-    elif z >= 0.016:
-        main.update()
-        z = 0
-    else:
-        z += 1 * dt
-
-    main.draw()
-
-    screen = pygame.transform.scale(screen, (640, 640))
-    display.blit(screen, (0, 0))
-    pygame.display.update()
+            self.screen = pygame.transform.scale(self.screen, (640, 640))
+            self.display.blit(self.screen, (0, 0))
+            pygame.display.update()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import engine
+import engine_beta as engine
 
 # variables
 paddle_y = 24
@@ -9,80 +9,91 @@ dx = 0.1
 dy = 0.1
 score = 0
 
+class begin:
+    def __init__(self,) -> None:
+        self.engine = engine.engine_()
+        
+        
+    # importing images and SFX into the game
+    def initialize(self):
+        # importing the images
+        self.engine.image_import("Assets/paddle.png", "paddle")
+        self.engine.image_import("Assets/ball.png", "ball")
 
-# importing images and SFX into the game
-def init():
-    # importing the images
-    engine.image_import("Assets/paddle.png", "paddle")
-    engine.image_import("Assets/ball.png", "ball")
-
-    # importing the SFX
-    engine.import_sfx("Assets/Hit.wav", "ball_hit")
-    engine.import_sfx("Assets/Score.wav", "score")
-
-
-# updating the game
-def update():
-    global paddle_y, paddle2_y, ball_x, ball_y, dx, dy, score
-
-    # checks if up arrow or down arrow is pressed and moves the paddle
-    if engine.key_down("K_UP"):
-        paddle_y -= 2
-    if engine.key_down("K_DOWN"):
-        paddle_y += 2
-
-    # locks the paddle so it cant go off-screen
-    if paddle_y < 0:
-        paddle_y = 0
-    if paddle_y + 16 > 64:
-        paddle_y = 48
-
-    # adds the ball velocity to the balls position
-    ball_x += dx
-    ball_y += dy
-
-    # moves the bot paddle
-    if ball_y < paddle2_y + 8:
-        paddle2_y -= 0.42
-    if ball_y > paddle2_y + 8:
-        paddle2_y += 0.42
-
-    # collisions with top, down, left and right side of the screen
-    if ball_y > 60:
-        dy = -0.5
-        engine.play_sfx("ball_hit")
-    if ball_y < 0:
-        dy = 0.5
-        engine.play_sfx("ball_hit")
-    if ball_x > 61:
-        dx = -0.5
-        ball_x = 30
-        ball_y = 30
-        score -= 1
-        engine.play_sfx("score")
-    if ball_x < 0:
-        dx = 0.5
-        ball_x = 30
-        ball_y = 30
-        score += 1
-        engine.play_sfx("score")
-
-    # collisions with the paddles
-    if ball_x > 54 and paddle_y < ball_y < paddle_y + 16:
-        dx = -0.5
-        engine.play_sfx("ball_hit")
-    if ball_x < 6 and paddle2_y < ball_y < paddle2_y + 16:
-        dx = 0.5
-        engine.play_sfx("ball_hit")
+        # importing the SFX
+        self.engine.import_sfx("Assets/Hit.wav", "ball_hit")
+        self.engine.import_sfx("Assets/Score.wav", "score")
 
 
-# draws the game
-def draw():
-    # makes the background dark blue
-    engine.draw_rect_filled(0, 0, 64, 64, (24, 20, 37))
-    # draws the score
-    engine.text(score, 30, 1)
-    # draws the paddles and ball
-    engine.draw("paddle", 58, paddle_y)
-    engine.draw("paddle", 2, paddle2_y)
-    engine.draw("ball", ball_x, ball_y)
+    # updating the game
+    def update(self):
+        global paddle_y, paddle2_y, ball_x, ball_y, dx, dy, score
+
+        # checks if up arrow or down arrow is pressed and moves the paddle
+        if self.engine.key_down("K_UP"):
+            paddle_y -= 2
+        if self.engine.key_down("K_DOWN"):
+            paddle_y += 2
+
+        # locks the paddle so it cant go off-screen
+        if paddle_y < 0:
+            paddle_y = 0
+        if paddle_y + 16 > 64:
+            paddle_y = 48
+
+        # adds the ball velocity to the balls position
+        ball_x += dx
+        ball_y += dy
+
+        # moves the bot paddle
+        if ball_y < paddle2_y + 8:
+            paddle2_y -= 0.42
+        if ball_y > paddle2_y + 8:
+            paddle2_y += 0.42
+
+        # collisions with top, down, left and right side of the screen
+        if ball_y > 60:
+            dy = -0.5
+            self.engine.play_sfx("ball_hit")
+        if ball_y < 0:
+            dy = 0.5
+            self.engine.play_sfx("ball_hit")
+        if ball_x > 61:
+            dx = -0.5
+            ball_x = 30
+            ball_y = 30
+            score -= 1
+            self.engine.play_sfx("score")
+        if ball_x < 0:
+            dx = 0.5
+            ball_x = 30
+            ball_y = 30
+            score += 1
+            self.engine.play_sfx("score")
+
+        # collisions with the paddles
+        if ball_x > 54 and paddle_y < ball_y < paddle_y + 16:
+            dx = -0.5
+            self.engine.play_sfx("ball_hit")
+        if ball_x < 6 and paddle2_y < ball_y < paddle2_y + 16:
+            dx = 0.5
+            self.engine.play_sfx("ball_hit")
+
+
+    # draws the game
+    def draw(self):
+        # makes the background dark blue
+        self.engine.draw_rect_filled(0, 0, 64, 64, (24, 20, 37))
+        # draws the score
+        self.engine.text(score, 30, 1)
+        # draws the paddles and ball
+        self.engine.draw("paddle", 58, paddle_y)
+        self.engine.draw("paddle", 2, paddle2_y)
+        self.engine.draw("ball", ball_x, ball_y)
+
+    def run(self):
+        self.engine.pass_down(self.initialize,self.draw,self.update)
+        self.engine.run()
+
+app = begin()
+app.run()
